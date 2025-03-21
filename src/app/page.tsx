8c68eb084193208d77db2fe2914a12aa88b8c6de
Meredith Allen <merallen@umich.edu>
2025-03-20 20:17:38 -0500

    add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      NATIONAL PARK PASSPORT
       <main className={styles.main}>
         <Image
           className={styles.logo}
